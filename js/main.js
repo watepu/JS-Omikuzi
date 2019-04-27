e25d773e485f349f6428f55e67d4fec31f5d7a60
watepu <40508321+watepu@users.noreply.github.com>
2019-04-28 05:28:37 +0900

    btnを押し込むことが出来るよう設定

--- js/main.js
+++ js/main.js
@@ -6,4 +6,10 @@
   btn.addEventListener('click', () => {
     btn.textContent = 'hit!';
   });
+  btn.addEventListener('mousedown', () => {
+    btn.classList.add('pressed');
+  });
+  btn.addEventListener('mouseup', () => {
+    btn.classList.remove('pressed');
+  });
 }
