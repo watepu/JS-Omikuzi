046a190edd327252afc2511ddeefde159f87b609
watepu <40508321+watepu@users.noreply.github.com>
2019-05-01 07:25:20 +0900

    おみくじの確率を変更

--- js/main.js
+++ js/main.js
@@ -4,9 +4,22 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    const results = ['大吉', '中吉', '凶', '小吉'];
-    const n = Math.floor(Math.random() * results.length);
-    btn.textContent = results[n];
+    // コメント部分は全ての確率が同じ場合
+    // const results = ['大吉', '中吉', '凶', '小吉'];
+    // const n = Math.floor(Math.random() * results.length);
+    // btn.textContent = results[n];
+    const n = Math.random();
+    if(n < 0.1){
+      btn.textContent = '大吉'; //10%
+    }else if(n < 0.3){
+      btn.textContent = '中吉';  //20%
+    }else if(n < 0.6){
+      btn.textContent = '小吉';  //30%
+    }else if(n < 0.9){
+      btn.textContent = '末吉';  //30%
+    }else{
+      btn.textContent = '凶';  //10%
+    }
   });
   btn.addEventListener('mousedown', () => {
     btn.classList.add('pressed');
