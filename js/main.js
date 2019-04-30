288799acfa51c6fa4836139fbfb64372fa2c62d0
watepu <40508321+watepu@users.noreply.github.com>
2019-04-30 22:22:43 +0900

    ランダムに生成された数値の部分の要素を表示させる

--- js/main.js
+++ js/main.js
@@ -6,17 +6,7 @@
   btn.addEventListener('click', () => {
     const results = ['大吉', '中吉', '凶'];
     const n = Math.floor(Math.random() * 3);
-    switch (n) {
-      case 0:
-      btn.textContent = '大吉';
-        break;
-      case 1:
-      btn.textContent = '中吉';
-        break;
-      case 2:
-      btn.textContent = '凶';
-        break;
-    }
+    btn.textContent = results[n];
   });
   btn.addEventListener('mousedown', () => {
     btn.classList.add('pressed');
