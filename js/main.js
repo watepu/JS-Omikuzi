71da14b7a720ea80f29bf6da13162eb695fcf958
watepu <40508321+watepu@users.noreply.github.com>
2019-05-01 07:12:46 +0900

    配列の長さが変わっても対応できるようlengthを使用

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    const results = ['大吉', '中吉', '凶'];
-    const n = Math.floor(Math.random() * 3);
+    const results = ['大吉', '中吉', '凶', '小吉'];
+    const n = Math.floor(Math.random() * results.length);
     btn.textContent = results[n];
   });
   btn.addEventListener('mousedown', () => {
