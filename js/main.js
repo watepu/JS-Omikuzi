373c4e606874703350cf4164590438fc19b0ed25
watepu <40508321+watepu@users.noreply.github.com>
2019-04-30 22:15:42 +0900

    おみくじ結果を配列に格納

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
+    const results = ['大吉', '中吉', '凶'];
     const n = Math.floor(Math.random() * 3);
     switch (n) {
       case 0:
